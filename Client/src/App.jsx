23ef73ad487b337a4a14d2363ad8f928fe80f5aa
Gaurav-0301 <gauravkakpure2006@gmail.com>
2026-03-30 23:41:49 +0530

    routes and axios setup successfully

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import { io } from "socket.io-client";
+import React, { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import SettingsPage from './Pages/SettingsPage';
+import ProfilePage from './Pages/ProfilePage';
+import SignupPage from './Pages/SignupPage';
+import LoginPage from './Pages/LoginPage';
+import { Loader } from "lucide-react";
+import { useAuthStore } from './Store/useAuthStore';
+
 const App = () => {
 
-  const socket=io("http://localhost:2724");
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth & !authUser) return (
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className="size-10 animate-spin" />
+    </div>
+  );
 
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={ authUser?<HomePage />:<Navigate to="/login"/>} />
+      <Route path="/signup" element={!authUser?<SignupPage />:<Navigate to="/"/>} />
+      <Route path="/settings" element={!authUser?<SettingsPage />:<Navigate to="/"/>} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={authUser?<ProfilePage />:<Navigate to="/login"/>} />
+    </Routes>
+  );
+};
 
-export default App
-
+export default App;
--- Server/server.js
+++ Server/server.js
@@ -4,14 +4,19 @@ require("dotenv").config();
 const authRouter=require("./src/routes/auth.route");
 const messageRouter=require("./src/routes/message.route")
 const {connectDB}=require("./src/config/db")
+const cors= require("cors")
 const app = express();
 connectDB();
 
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
 
+}));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/",authRouter);
+app.use("/auth",authRouter);
 app.use("/msg",messageRouter);
 
 
