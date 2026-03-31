4a78d83f5a02e559044454d0725ea9953d101ec5
Gaurav-0301 <gauravkakpure2006@gmail.com>
2026-03-31 11:53:19 +0530

    Navbar completed

--- Client/src/Pages/HomePage.jsx
+++ Client/src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 
 const HomePage = () => {
   return (
-    <div>
+    <>
+    <Navbar/>
       <h1>HomePage</h1>
-    </div>
+    </>
   )
 }
 
